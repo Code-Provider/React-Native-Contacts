07c6696ef1c5e15a98ab5ae2e00cfa20d37ea74b
Imad Zinbi <imadzinbi22@gmail.com>
2021-01-05 01:28:22 +0200

    add ContactScreen

---
+++ src/screens/ContactScreen.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { TouchableOpacity, StyleSheet, View } from 'react-native'
+import { Text } from 'react-native-paper'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import TextInput from '../components/TextInput'
+import BackButton from '../components/BackButton'
+import { theme } from '../core/theme'
+
+
+const ContactScreen = ({ navigation }) => {
+    return (
+        <Background>
+        <Logo />
+        <Header>Contacts</Header>
+        </Background>
+    )
+}
+
+    const styles = StyleSheet.create({
+    forgotPassword: {
+        width: '100%',
+        alignItems: 'flex-end',
+        marginBottom: 24,
+    },
+    row: {
+        flexDirection: 'row',
+        marginTop: 4,
+    },
+    forgot: {
+        fontSize: 13,
+        color: theme.colors.secondary,
+    },
+    link: {
+        fontWeight: 'bold',
+        color: theme.colors.primary,
+    },
+    })
+
+export default ContactScreen
